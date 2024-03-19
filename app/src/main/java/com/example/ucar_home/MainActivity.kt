c5b3727c83f1dba4c39e6b08a82d0ba932c674b7
luitor238 <luistorgacastro@gmail.com>
2024-03-19 13:58:02 +0100

    bottom navigation terminado y funcionando correctamente toolbar a falta de algunas cosas

--- app/src/main/java/com/example/ucar_home/MainActivity.kt
+++ app/src/main/java/com/example/ucar_home/MainActivity.kt
@@ -3,69 +3,88 @@ package com.example.ucar_home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageButton
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.example.ucar_home.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    val homeFragment = HomeFragment()
-    val searchFragment = SearchFragment()
-    val mapsFragment = MapsFragment()
-    val chatFragment = ChatFragment()
+    private val homeFragment = HomeFragment()
+    private val searchFragment = SearchFragment()
+    private val mapsFragment = MapsFragment()
+    private val chatFragment = ChatFragment()
+
+    // Mapa para mantener las referencias a los íconos originales
+    private val originalIconsMap = mutableMapOf<Int, Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        //FUNCIONALIDAD TOOLBAR
+        // Inicializar los íconos originales
+        originalIconsMap[R.id.homeFragment] = R.drawable.icon_home
+        originalIconsMap[R.id.searchFragment] = R.drawable.icon_search
+        originalIconsMap[R.id.mapsFragment] = R.drawable.icon_maps
+        originalIconsMap[R.id.chatFragment] = R.drawable.icon_chat
+
+        // Funcionalidad del toolbar
         toolbar()
 
-        //ELECCION DE FRAGMENT
-        binding.bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-
-        //CARGAR FRAGMENT INICIO
+        // Cargar el fragmento de inicio
         loadFragment(homeFragment)
 
+        // Establecer el listener para la selección de elementos del menú
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
-
-    // FUNCION PARA CARGAR LOS DISTINTOS FRAGMENTS
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.homeFragment -> {
                 loadFragment(homeFragment)
+                item.setIcon(R.drawable.icon_home_bold)
+                restoreOriginalIcon(item)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.searchFragment -> {
                 loadFragment(searchFragment)
+                item.setIcon(R.drawable.icon_search_bold)
+                restoreOriginalIcon(item)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.mapsFragment -> {
                 loadFragment(mapsFragment)
+                item.setIcon(R.drawable.icon_maps_bold)
+                restoreOriginalIcon(item)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.chatFragment -> {
                 loadFragment(chatFragment)
+                item.setIcon(R.drawable.icon_chat_bold)
+                restoreOriginalIcon(item)
                 return@OnNavigationItemSelectedListener true
             }
             else -> return@OnNavigationItemSelectedListener false
         }
     }
 
-    // FUNCION PARA CARGAR FRAGMENT DE INICIO
-    fun loadFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frame_container, fragment)
-        transaction.commit()
+    // Función para cargar un fragmento en el contenedor
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+
+    // Función para restaurar el ícono original del elemento previamente seleccionado
+    private fun restoreOriginalIcon(selectedItem: MenuItem) {
+        val itemId = selectedItem.itemId
+        originalIconsMap.keys.forEach { id ->
+            if (id != itemId) {
+                binding.bottomNavigation.menu.findItem(id)?.setIcon(originalIconsMap[id]!!)
+            }
+        }
     }
 
     // FUNCION PARA LA FUNCIONALIDAD DEL TOOLBAR
