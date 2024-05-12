3a8ba9c2d4d06d2cafb447501ef0e50a87bcba5d
MarcoGarciaa <margar306@alu.centroafuera.es>
2024-05-12 17:23:15 +0200

    mejoras

--- app/src/main/java/com/example/ucar_home/CreateEventActivity.kt
+++ app/src/main/java/com/example/ucar_home/CreateEventActivity.kt
@@ -1,11 +1,16 @@
 package com.example.ucar_home
 
+import android.content.ContentValues
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ucar_home.databinding.ActivityCreateEventBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -37,7 +42,10 @@ class CreateEventActivity : AppCompatActivity() {
 
                 if (date != null) {
                     val event = EventObject(title, imageUrl, date, address, description)
+
                     // Aquí puedes hacer lo que quieras con el evento, como guardarlo en una base de datos, etc.
+                    saveEventToDatabase(event.title, event.imageUrl, event.date, event.address, event.description)
+
                     Toast.makeText(this, "Event created: $event", Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(this, "Invalid date format", Toast.LENGTH_SHORT).show()
@@ -45,6 +53,28 @@ class CreateEventActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+    private fun saveEventToDatabase(
+        title: String?,
+        imageUrl: String?,
+        date: LocalDate?,
+        address: String?,
+        description: String?
+    ) {
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+        val database = FirebaseDatabase.getInstance().reference
+        val event = EventObject(title!!, imageUrl!!, date!!, address!!, description!! )
+        uid?.let {
+
+            database.child("events").child(it).setValue(event)
+                .addOnSuccessListener {
+                    Log.d(ContentValues.TAG, "User data saved successfully.")
+                    // Proceed to next activity or whatever you need to do
+                }
+                .addOnFailureListener { e ->
+                    Log.d(ContentValues.TAG, "Error saving user data: ${e.message}")
+                }
         }
     }
 }
